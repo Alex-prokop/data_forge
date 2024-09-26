393d9c11b77dddf3f50c7ca6f35f965c2d30e6fb
Alexandr Prokopovich <prokop7@mail.ru>
2024-09-26 15:56:27 +0300

    Update Dockerfiles for frontend and backend configurations

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,7 @@ import cors from 'cors';
 import { getData, exportData } from './controllers/dataController';
 
 const app = express();
-app.use(
-  cors({
-    origin: 'http://localhost:3000', // Разрешаем запросы только с вашего фронтенда
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get('/data', getData);
