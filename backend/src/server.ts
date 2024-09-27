1ac980f679d5173b8de46a744b3a49755aefc77f
Alexandr Prokopovich <prokop7@mail.ru>
2024-09-27 14:06:54 +0300

    fix: nixpacks

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,35 +1,10 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import { getData, exportData } from './controllers/dataController';
+import dotenv from 'dotenv';
+import app from './app';
 
-const app = express();
+dotenv.config();
 
-// Настройка CORS без указания origin
-const corsOptions = {
-  origin: (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean) => void
-  ) => {
-    const allowedOrigins = [
-      'https://dataforge-production.up.railway.app',
-      'http://localhost:3000',
-    ]; // Добавьте ваши разрешенные домены
+const PORT: number = Number(process.env.PORT) || 5000;
 
-    if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-app.get('/data', getData);
-app.get('/export', exportData);
-
-export default app;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
